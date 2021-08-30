0f707f501cbdfd707f8b4d73359fbdf6b9e8f878
snapdrammer <89353229+snapdrammer@users.noreply.github.com>
2021-08-30 17:55:11 +0100

    Update clock-edger.js

--- js/clock-edger.js
+++ js/clock-edger.js
@@ -93,7 +93,11 @@
 
     linkElement.title = "Link to MSc Disseration";
     linkElement.href = "../thesis.pdf";
+    linkElement.class = "image fit";
+    linkElement.src = "../thesis.pdf";
 
+    
+//     <a href="pdfs/ProjMarr_slides.pdf" class="image fit"><img src="images/marr_pic.jpg" alt=""></a>
     const existingText = document.getElementById("introPara");
     existingText.textContent = "";
     title.appendChild(linkElement);
